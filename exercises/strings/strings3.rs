05686d5d08c6223a14da1ee49bf1c26c3c2c681b
Jianyong Wu <wujianyong@hygon.cn>
2024-09-28 14:24:23 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,61 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let tmp: Vec<char> = input.chars().collect();
+    let mut ret = String::new();
+    let mut flag = 0;
+    for i in 0..tmp.len() {
+	if flag == 0 && tmp[i] == ' ' {
+		continue;
+	}
+	flag = 1;
+	if tmp[i] != ' ' || i < tmp.len() - 1 && tmp[i + 1] != ' ' {
+		ret.push(tmp[i]);
+	}
+    }
+    ret.to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // TODO: Add " world!" to the string! There's mdultiple ways to do this!
+    let mut ret = input.to_string();
+    let s = " world!".to_string();
+    ret += &s;
+    ret
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let tmp: Vec<char> = input.chars().collect();
+    let mut ret = String::new();
+    let mut j = 0;
+    let mut i = 0;
+    let cars = ['c', 'a', 'r', 's'];
+    let balloons: Vec<char> = "balloons".to_string().chars().collect();
+    while i < tmp.len() {
+	if tmp[i] != 'c' {
+		ret.push(tmp[i]);
+		i += 1;
+		continue;
+	}
+        while j < 4 && i < tmp.len() && tmp[i] == cars[j] {
+		i += 1;
+		j += 1;
+	}
+	if j == 4 {
+		j = 0;
+        	for c in &balloons {
+			ret.push(*c);
+		}
+	} else {
+		i -= j;
+		ret.push(tmp[i]);
+		i += 1;
+	}
+    }
+    ret.to_string()
 }
 
 #[cfg(test)]
