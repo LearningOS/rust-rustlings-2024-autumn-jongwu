85706ae455fbb1ded1ae8f05e54dec8f0c866015
Jianyong Wu <jianyong.wu@outlook.com>
2024-10-04 21:49:10 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,20 +30,69 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let o: String = match command {
+                Command::Uppercase => make_upper(string.to_string()),
+                Command::Trim => make_trim(string.to_string()),
+                Command::Append(sz) => make_append(string.to_string(), *sz),
+            };
+            output.push(o);
         }
         output
+    }
+
+    fn make_upper(s:String) -> String {
+        let mut out = String::new();
+        let mut ch: char;
+        for c in s.as_bytes() {
+            if *c as u8 >= 'a' as u8 && *c as u8 <= 'z' as u8 {
+                ch = (*c as u8 + 'A' as u8 - 'a' as u8) as char;
+            } else {
+                ch = *c as char;
+            }
+            out.push(ch);
+        }
+        out
+    }
+
+    fn make_trim(s: String) -> String {
+        let mut flag = 0;
+        let mut out = String::new();
+        let mut i = 0;
+        for c in s.chars() {
+            i += 1;
+            if c == ' ' && flag == 0 {
+                continue;
+            }
+            flag = 1;
+            if c == ' ' && (i == s.len() || out.clone().into_bytes()[out.len() - 1] == b' ') {
+                break;
+            }
+            out.push(c);
+        }
+        out
+    }
+
+    fn make_append(s: String, sz: usize) -> String {
+        let mut out = String::new();
+        let mut i: usize = 0;
+        out.push_str(s.as_str());
+        while i < sz {
+            out.push_str("bar");
+            i += 1;
+        }
+        out
     }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
